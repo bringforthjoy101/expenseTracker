7d1689abe0ae03887c30ceb0b868bfc9e15b5b92
sahmie <sahmienwoko@gmail.com>
2020-07-16 13:01:20 +0000

    ...

--- helpers/checkExpenseStatus.js
+++ helpers/checkExpenseStatus.js
@@ -1,16 +1,17 @@
 function checkExpenseStatus (req, res, expenseStatus) {
 console.log('this is the inputed status ' + `${expenseStatus}`);
-  if (expenseStatus != 'Pending') {
+  if (expenseStatus == 'Pending') {
+    
+        return expenseStatus;
+        
+    } else {
         console.log('Operation denied, expense reviewed already!');
+        console.log('this is the status: ' + expenseStatus);
         return res.status(401).send({
             status: false,
             message: 'Operation declined, expense reviewed already! ' + expenseStatus
         });
     }
-    console.log('Operation denied, expense reviewed already!');
-    console.log('this is the status: ' + expenseStatus);
-  
-  return expenseStatus;
   
 }
 
