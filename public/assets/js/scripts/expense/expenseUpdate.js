7b76a1caeadba49cb551ccdbddc305f2760b4155
Bringforthjoy101 <adelugba.emma@gmail.com>
2020-08-12 12:35:13 +0000

    ...

--- public/assets/js/scripts/expense/expenseUpdate.js
+++ public/assets/js/scripts/expense/expenseUpdate.js
@@ -90,29 +90,41 @@ const updateExpense = async (data, expenseId) => {
     }
 };
 const deleteExpense = async (expenseId) => {
-    try {
-        const expense = await fetch(`${Route.apiRoot}/expense/${expenseId}/delete`, {
-            // mode: 'no-cors',
-            method: 'POST'
-        });
-        let response = await expense.json();
-        if (response.status) {
-            swal.fire(
-                'Sucessful!',
-                'Expense deleted successfully!',
-                'success'
-            )
-            location.href = `/expenses`;
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "Your action cannot be reversible!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, Delete it!'
+    }).then(async (result) => {
+        if (result.value) {
+            try {
+                const expense = await fetch(`${Route.apiRoot}/expense/${expenseId}/delete`, {
+                    // mode: 'no-cors',
+                    method: 'POST'
+                });
+                let response = await expense.json();
+                if (response.status) {
+                    swal.fire(
+                        'Sucessful!',
+                        'Expense deleted successfully!',
+                        'success'
+                    )
+                    location.href = `/myExpenses`;
+                }
+            } catch (error) {
+                console.log(error);
+                // show network error notification
+                swal.fire(
+                    'Oops!',
+                    'An error was encountered! Please review your network connection.',
+                    'error'
+                )
+            }
         }
-    } catch (error) {
-        console.log(error);
-        // show network error notification
-        swal.fire(
-            'Oops!',
-            'An error was encountered! Please review your network connection.',
-            'error'
-        )
-    }
+    });
 }
 
 const onActionCompleted = (approval) => {
